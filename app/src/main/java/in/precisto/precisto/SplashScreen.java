b717bbf718665ad3509726ddea5bd547c568980d
Ruchika <ruchikashelke6475@gmail.com>
2019-04-13 19:13:14 +0530

    SplashScreen created.

---
+++ app/src/main/java/in/precisto/precisto/SplashScreen.java
@@ -0,0 +1,28 @@
+package in.precisto.precisto;
+
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import gr.net.maroulis.library.EasySplashScreen;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EasySplashScreen easySplashScreen = new EasySplashScreen(SplashScreen.this)
+                .withFullScreen()
+                .withTargetActivity(MainActivity.class)
+                .withSplashTimeOut(4000)
+                .withBackgroundColor(Color.parseColor("#3B5998"))
+                .withLogo(R.mipmap.ic_launcher_round);
+
+        View view = easySplashScreen.create();
+
+        setContentView(view);
+
+
+    }
+}
