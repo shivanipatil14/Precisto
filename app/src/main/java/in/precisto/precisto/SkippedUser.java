786e2a319bc02de28fcddb7c23c09c12ec586da9
Sanket <sanket.ingale1998@gmail.com>
2019-04-14 18:10:36 +0530

    skipped user drawer added precisto colored logo added

--- app/src/main/java/in/precisto/precisto/SkippedUser.java
+++ app/src/main/java/in/precisto/precisto/SkippedUser.java
@@ -8,64 +8,52 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+public class SkippedUser extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private DrawerLayout drawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_skipped_user);
 
-        Toolbar toolbar = findViewById(R.id.main_toolbar);
+        Toolbar toolbar = findViewById(R.id.skipped_toolbar);
         setSupportActionBar(toolbar);
 
-        drawer = findViewById(R.id.main_drawer_layout);
+        drawer = findViewById(R.id.skipped_drawer_layout);
 
-        NavigationView navigationView = findViewById(R.id.main_drawer_view);
+        NavigationView navigationView = findViewById(R.id.skipped_drawer_view);
         navigationView.setNavigationItemSelectedListener(this);
 
         ActionBarDrawerToggle toggle =  new ActionBarDrawerToggle(this,drawer,toolbar,
                 R.string.navigation_drawer_open,R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
-
     }
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
 
         switch (menuItem.getItemId()) {
-            case R.id.drawer_home:
-                Toast.makeText(this, "Home", Toast.LENGTH_SHORT).show();
-                break;
-            case R.id.drawer_profile:
-                Toast.makeText(this, "Profile", Toast.LENGTH_SHORT).show();
-                break;
-            case R.id.drawer_services:
+            case R.id.drawer_services_skipped:
                 Toast.makeText(this, "Services", Toast.LENGTH_SHORT).show();
                 break;
-            case R.id.drawer_quotation:
-                Toast.makeText(this, "Quotations", Toast.LENGTH_SHORT).show();
-                break;
-            case R.id.drawer_faq:
+            case R.id.drawer_faq_skipped:
                 Toast.makeText(this, "FAQs", Toast.LENGTH_SHORT).show();
                 break;
-            case R.id.drawer_contact:
+            case R.id.drawer_contact_skipped:
                 Toast.makeText(this, "Contact us", Toast.LENGTH_SHORT).show();
                 break;
-            case R.id.drawer_about:
+            case R.id.drawer_about_skipped:
                 Toast.makeText(this, "About us", Toast.LENGTH_SHORT).show();
                 break;
-            case R.id.drawer_tnc:
+            case R.id.drawer_tnc_skipped:
                 Toast.makeText(this, "Terms and Conditions", Toast.LENGTH_SHORT).show();
                 break;
-            case R.id.drawer_privacy_policy:
+            case R.id.drawer_privacy_policy_skipped:
                 Toast.makeText(this, "Privacy Policies", Toast.LENGTH_SHORT).show();
                 break;
 
@@ -84,24 +72,5 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             super.onBackPressed();
         }
 
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater menuInflater = getMenuInflater();
-        menuInflater.inflate(R.menu.main_action_menu,menu);
-        return super.onCreateOptionsMenu(menu);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        super.onOptionsItemSelected(item);
-        if(item.getItemId() == R.id.action_logout) {
-            Toast.makeText(this, "Logout", Toast.LENGTH_SHORT).show();
-        } else if (item.getItemId() == R.id.action_exit) {
-            Toast.makeText(this, "Exit", Toast.LENGTH_SHORT).show();
-        }
-        return true;
     }
 }
