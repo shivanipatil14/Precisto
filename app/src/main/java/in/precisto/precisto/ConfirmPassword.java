e2cdf636f89d9f42cdc7dde3fd1aa9dcd6d6d139
umairq5 <49568789+umairq5@users.noreply.github.com>
2019-04-16 20:21:30 +0530

    ConfirmPassword

---
+++ app/src/main/java/in/precisto/precisto/ConfirmPassword.java
@@ -0,0 +1,16 @@
+package in.precisto.precisto;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class ConfirmPassword extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_confirm_password);
+    }
+    public void confirmPassword(View view)
+    {}
+}
