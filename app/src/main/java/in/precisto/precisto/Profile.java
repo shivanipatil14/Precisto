fc8890873c8600a5634616c177152bd41a8c5f66
Sanket <sanket.ingale1998@gmail.com>
2019-04-21 20:27:14 +0530

    edit profile

--- app/src/main/java/in/precisto/precisto/Profile.java
+++ app/src/main/java/in/precisto/precisto/Profile.java
@@ -7,13 +7,52 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 public class Profile extends Fragment{
+
+    private EditText fname,lname,contact,dob,email,gen,bname,industry,btype;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view=inflater.inflate(R.layout.fragment_profile,container,false);
+
+        fname=(EditText)view.findViewById(R.id.et_profile_fname);
+        lname=(EditText)view.findViewById(R.id.et_profile_lname);
+        contact=(EditText)view.findViewById(R.id.et_profile_contact);
+        dob=(EditText)view.findViewById(R.id.et_profile_dob);
+        email=(EditText)view.findViewById(R.id.et_profile_email);
+        gen=(EditText)view.findViewById(R.id.et_profile_gen);
+        bname=(EditText)view.findViewById(R.id.et_profile_business_name);
+        industry=(EditText)view.findViewById(R.id.et_profile_industry);
+        btype=(EditText)view.findViewById(R.id.et_profile_business_type);
+
+        fname.setEnabled(false);
+        lname.setEnabled(false);
+        contact.setEnabled(false);
+        dob.setEnabled(false);
+        email.setEnabled(false);
+        gen.setEnabled(false);
+        bname.setEnabled(false);
+        industry.setEnabled(false);
+        btype.setEnabled(false);
+
         return view;
+    }
+
+    public void edit(View view) {
+
+        fname.setEnabled(true);
+        lname.setEnabled(true);
+        contact.setEnabled(true);
+        dob.setEnabled(true);
+        email.setEnabled(true);
+        gen.setEnabled(true);
+        bname.setEnabled(true);
+        industry.setEnabled(true);
+        btype.setEnabled(true);
+
     }
 
 }
